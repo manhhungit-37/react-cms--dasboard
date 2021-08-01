90c82f73bf6bf89a9c2cfac54b371c384bd9498b
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-08-01 11:00:54 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+// ant core
+import { Layout, Menu } from 'antd';
+
+// ant icon
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(true)
+
+  function toggle() {
+    setCollapsed(prevState => !prevState)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: toggle,
+          })}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+// views
+import Login from 'views/Login/Login';
+import NotFound from 'views/NotFound/NotFound';
+
+// styles
+import 'antd/dist/antd.css';
+import 'index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  </Router> ,
   document.getElementById('root')
 );
 
